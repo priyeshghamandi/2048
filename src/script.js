2e15f8bcaa61a69f7b6267ad463c0ad4a7655066
priyesh <priyesh.ghamandi@questionpro.com>
2022-04-04 18:53:24 +0530

    swipe

--- src/script.js
+++ src/script.js
@@ -3,13 +3,78 @@ import Tile from './Tile';
 
 const gameBoard = document.getElementById('game-board');
 const grid = new Grid(gameBoard);
+let xDown = null;                                                        
+let yDown = null;
 grid.randomEmptyCell().tile = new Tile(gameBoard);
 grid.randomEmptyCell().tile = new Tile(gameBoard);
 
 setupInput();
 function setupInput() {
     window.addEventListener('keydown', handleInput, {once: true});
+    document.addEventListener('touchstart', handleTouchStart, false);        
+    document.addEventListener('touchmove', handleTouchMove, false);
 }
+
+function getTouches(evt) {
+    return evt.touches ||             // browser API
+           evt.originalEvent.touches; // jQuery
+  }  
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};  
+
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+                                                                         
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
+        if ( xDiff > 0 ) {
+            if(!canMoveRight()) {
+                setupInput();
+                return;                
+            }
+            await moveRight();
+            break;
+        } else {
+            if(!canMoveLeft()) {
+                setupInput();
+                return;                
+            }
+            await moveLeft();
+            break;
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            if(!canMoveDown()) {
+                setupInput();
+                return;                
+            }
+            await moveDown();
+            break;
+        } else { 
+            if(!canMoveUp()) {
+                setupInput();
+                return;                
+            }
+            await moveUp();
+            break;
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
 
 async function handleInput(e) {
     switch (e.key) {
