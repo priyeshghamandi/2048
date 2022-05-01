f6984d8b6464d760ae829909b74a7c14be6911de
priyesh <priyesh.ghamandi@questionpro.com>
2022-05-01 09:39:44 +0530

    Added touch support - swipes

--- src/script.js
+++ src/script.js
@@ -3,78 +3,40 @@ import Tile from './Tile';
 
 const gameBoard = document.getElementById('game-board');
 const grid = new Grid(gameBoard);
-let xDown = null;                                                        
-let yDown = null;
 grid.randomEmptyCell().tile = new Tile(gameBoard);
 grid.randomEmptyCell().tile = new Tile(gameBoard);
 
 setupInput();
 function setupInput() {
     window.addEventListener('keydown', handleInput, {once: true});
-    document.addEventListener('touchstart', handleTouchStart, false);        
-    document.addEventListener('touchmove', handleTouchMove, false);
+    window.addEventListener('touchstart', handleTouchStart, false);
+    window.addEventListener('touchmove', handleTouchMove, false);
+    window.addEventListener('touchend', handleTouchEnd, false);
+
+    window.addEventListener('swiped-left',  handleLeft, {once: true});
+    window.addEventListener('swiped-right',  hadnleRight, {once: true});
+    window.addEventListener('swiped-up',  hadnleUp, {once: true});
+    window.addEventListener('swiped-down',  hadnleDown, {once: true});
+
 }
 
-function getTouches(evt) {
-    return evt.touches ||             // browser API
-           evt.originalEvent.touches; // jQuery
-  }  
+async function handleLeft() {  
+     handleInput({key: 'ArrowLeft'})
+}
 
-function handleTouchStart(evt) {
-    const firstTouch = getTouches(evt)[0];                                      
-    xDown = firstTouch.clientX;                                      
-    yDown = firstTouch.clientY;                                      
-};  
+async function hadnleRight() {
+    handleInput({key: 'ArrowRight'})
+}
+
+async function hadnleUp() { 
+    handleInput({key: 'ArrowUp'})
+}
+
+async function hadnleDown() { 
+    handleInput({key: 'ArrowDown'})
+}
 
 
-function handleTouchMove(evt) {
-    if ( ! xDown || ! yDown ) {
-        return;
-    }
-
-    var xUp = evt.touches[0].clientX;                                    
-    var yUp = evt.touches[0].clientY;
-
-    var xDiff = xDown - xUp;
-    var yDiff = yDown - yUp;
-                                                                         
-    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
-        if ( xDiff > 0 ) {
-            if(!canMoveRight()) {
-                setupInput();
-                return;                
-            }
-            await moveRight();
-            break;
-        } else {
-            if(!canMoveLeft()) {
-                setupInput();
-                return;                
-            }
-            await moveLeft();
-            break;
-        }                       
-    } else {
-        if ( yDiff > 0 ) {
-            if(!canMoveDown()) {
-                setupInput();
-                return;                
-            }
-            await moveDown();
-            break;
-        } else { 
-            if(!canMoveUp()) {
-                setupInput();
-                return;                
-            }
-            await moveUp();
-            break;
-        }                                                                 
-    }
-    /* reset values */
-    xDown = null;
-    yDown = null;                                             
-};
 
 async function handleInput(e) {
     switch (e.key) {
@@ -195,8 +157,98 @@ function slideTiles(cells) {
             return promises;
         })
 
-    )
-
-
-    
+    )    
 }
+
+let xDown = null;
+let yDown = null;
+let xDiff = null;
+let yDiff = null;
+let timeDown = null;
+let startEl = null;
+
+function handleTouchEnd(e) {
+    if (startEl !== e.target) return;
+
+    let swipeThreshold = parseInt(getNearestAttribute(startEl, 'data-swipe-threshold', '20'), 10); // default 20px
+    let swipeTimeout = parseInt(getNearestAttribute(startEl, 'data-swipe-timeout', '500'), 10);    // default 500ms
+    let timeDiff = Date.now() - timeDown;
+    let eventType = '';
+    let changedTouches = e.changedTouches || e.touches || [];
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) { 
+        if (Math.abs(xDiff) > swipeThreshold && timeDiff < swipeTimeout) {
+            if (xDiff > 0) {
+                eventType = 'swiped-left';
+            }
+            else {
+                eventType = 'swiped-right';
+            }
+        }
+    }
+    else if (Math.abs(yDiff) > swipeThreshold && timeDiff < swipeTimeout) {
+        if (yDiff > 0) {
+            eventType = 'swiped-up';
+        }
+        else {
+            eventType = 'swiped-down';
+        }
+    }
+
+    if (eventType !== '') {
+
+        let eventData = {
+            dir: eventType.replace(/swiped-/, ''),
+            touchType: (changedTouches[0] || {}).touchType || 'direct',
+            xStart: parseInt(xDown, 10),
+            xEnd: parseInt((changedTouches[0] || {}).clientX || -1, 10),
+            yStart: parseInt(yDown, 10),
+            yEnd: parseInt((changedTouches[0] || {}).clientY || -1, 10)
+        };
+        
+        startEl.dispatchEvent(new CustomEvent('swiped', { bubbles: true, cancelable: true, detail: eventData }));
+        startEl.dispatchEvent(new CustomEvent(eventType, { bubbles: true, cancelable: true, detail: eventData }));
+    }
+
+    // reset values
+    xDown = null;
+    yDown = null;
+    timeDown = null;
+}
+
+function handleTouchStart(e) {
+    if (e.target.getAttribute('data-swipe-ignore') === 'true') return;
+
+    startEl = e.target;
+
+    timeDown = Date.now();
+    xDown = e.touches[0].clientX;
+    yDown = e.touches[0].clientY;
+    xDiff = 0;
+    yDiff = 0;
+}
+function handleTouchMove(e) {
+    if (!xDown || !yDown) return;
+
+    let xUp = e.touches[0].clientX;
+    let yUp = e.touches[0].clientY;
+
+    xDiff = xDown - xUp;
+    yDiff = yDown - yUp;
+}
+
+function getNearestAttribute(el, attributeName, defaultValue) {
+    while (el && el !== document.documentElement) {
+
+        let attributeValue = el.getAttribute(attributeName);
+
+        if (attributeValue) {
+            return attributeValue;
+        }
+
+        el = el.parentNode;
+    }
+
+    return defaultValue;
+}
+
